d2f42993a4499b464eb076f83c80aff800baf0fd
Pryz <ju.pryz@gmail.com>
2015-01-27 16:42:56 -0800

    refactor Landrush provisioner

--- lib/config_builder/model/provisioner/landrush.rb
+++ lib/config_builder/model/provisioner/landrush.rb
@@ -1,20 +1,22 @@
 # @see https://github.com/phinze/landrush 
 class ConfigBuilder::Model::Provisioner::Landrush < ConfigBuilder::Model::Base
 
-  # @!attribute [rw] hosts
-  #   @return [Hosts] blabla 
-  LANDRUSH_ATTRIBUTES = [:hosts, :enabled, :tld, :upstream_servers, :host_ip_address, :guest_redirect_dns]
-
-  LANDRUSH_ATTRIBUTES.each do |attr|
-    def_model_attribute attr
-  end
+  def_model_attribute :hosts
+  def_model_attribute :enabled
+  def_model_attribute :tld
+  def_model_attribute :upstream_servers
+  def_model_attribute :hosts_ip_address
+  def_model_attribute :guest_redirect_dns
 
   def to_proc
     Proc.new do |vm_config|
       vm_config.provision :landrush do |landrush_config|
-        LANDRUSH_ATTRIBUTES.each do |key|
-          config.send("#{key}=", attr(key)) if attr(key)
-        end
+        landrush_config.hosts = attr(:hosts) if attr(:hosts)
+        landrush_config.hosts = attr(:enabled) if attr(:enabled)
+        landrush_config.hosts = attr(:tld) if attr(:tld)
+        landrush_config.hosts = attr(:upstream_servers) if attr(:upstream_servers)
+        landrush_config.hosts = attr(:hosts_ip_address) if attr(:hosts_ip_address)
+        landrush_config.hosts = attr(:guest_redirect_dns) if attr(:guest_redirect_dns)
       end
     end
   end
